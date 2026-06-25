5fa77ee33b79b84e5b280116ffb60b34cce1f862
Natnael Deribe <88533508+Natink1@users.noreply.github.com>
2026-06-25 09:25:28 +0300

    Vercel Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import viteReact from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { nitro } from "nitro/vite";
 import { fileURLToPath, URL } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tanstackStart(), nitro(), viteReact()],
+  plugins: [tanstackStart(), nitro(), viteReact(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
