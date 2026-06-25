bfab05e6b0a07d498e379c69a4e296948c829b6c
Natnael Deribe <88533508+Natink1@users.noreply.github.com>
2026-06-25 09:10:49 +0300

    Vercel Fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Services from "@/components/Services";
-import Skills from "@/components/Skills";
-import Works from "@/components/Works";
-import Experience from "@/components/Experience";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Services from "../components/Services";
+import Skills from "../components/Skills";
+import Works from "../components/Works";
+import Experience from "../components/Experience";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 export const Route = createFileRoute("/")({
   head: () => ({
